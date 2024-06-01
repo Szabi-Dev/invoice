2c535736f256d3fcc7ae5ff0f9af79ad50317dd9
szabolcs <szabolcs.csorgo@ciit.eu>
2024-06-01 15:09:08 +0200

    input for year and month

--- src/main.py
+++ src/main.py
@@ -4,10 +4,14 @@ from services.config_parser import config_parser
 from services.date.date_service import get_monthly_date_interval
 from services.youtrack import sprint_service
 
-
 config = config_parser.parse_youtrack_config("src/config/dlouhy.ini");
 
-date = get_monthly_date_interval(2024,5);
+print('Year:')
+year = int(input())
+print('Month:')
+month = int(input())
+
+date = get_monthly_date_interval(year,month);
 sprints = sprint_service.get_sprints_in_interval(config, date);
 print(sprints);
 

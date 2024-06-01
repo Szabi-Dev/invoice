d95ba298a3dfd4bc749dc57eb186b1ac5e3bb982
szabolcs <szabolcs.csorgo@ciit.eu>
2024-06-01 15:04:48 +0200

    affected sprints

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 
 
 from services.config_parser import config_parser
+from services.date.date_service import get_monthly_date_interval
 from services.youtrack import sprint_service
 
 
 config = config_parser.parse_youtrack_config("src/config/dlouhy.ini");
 
-sprints = sprint_service.get_sprints(config);
+
+date = get_monthly_date_interval(2024,5);
+sprints = sprint_service.get_sprints_in_interval(config, date);
+print(sprints);
 
 
 
--- src/services/youtrack/sprint_service.py
+++ src/services/youtrack/sprint_service.py
@@ -4,13 +4,25 @@ from services.url import url_builder
 
 SPRINT_URL =  "/agiles/{projectId}/sprints?"
 
-PARAM_TOP = "top";
+PARAM_TOP = "$top";
 PARAM_TOP_VALUE = 1000;
 
 PARAM_FIELDS = "fields"; 
 PARAM_FIELDS_VALUE = "id,name,start,finish";
 
+def get_sprints_in_interval(youtrack_config, datemodel):
+    response = get_sprints(youtrack_config);
+    sprints = [];
 
+    for e in response:
+        if e['finish'] != None and e['start'] < datemodel.end_in_ms and e['finish'] > datemodel.start_in_ms:
+            sprints.append(e);
+    #sprints.sort(key=sortSprint);
+    #sprints[0]['start'] = datemodel.start_in_ms;
+    #sprints[-1]['finish'] = datemodel.end_in_ms;
+    #for sp in sprints:
+    #    sp['finish'] = sp['finish'] - 86400000;
+    return sprints;  
 
 def get_sprints(youtrack_config):
     postfix = SPRINT_URL.replace("{projectId}", str(youtrack_config.project));
@@ -18,6 +30,11 @@ def get_sprints(youtrack_config):
     url = youtrack_config.url + postfix;
     url = url_builder.add_param_to_url(url, PARAM_FIELDS, str(PARAM_FIELDS_VALUE));
     url = url_builder.add_param_to_url(url, PARAM_TOP, str(PARAM_TOP_VALUE));
+    print(url)
     response = requests.get(url, headers=Headers);
-    print(response.json());
+    return response.json();
+
+def sortSprint(e):
+    return e['start']
+
 

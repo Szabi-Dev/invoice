05bbc42985d8399b783e9514a90c52645b2ea17a
szabolcs <szabolcs.csorgo@ciit-software.com>
2024-06-04 20:34:25 +0200

    colored output

--- src/youtrack_statistics_sprint.py
+++ src/youtrack_statistics_sprint.py
@@ -4,6 +4,12 @@ from services.config_parser import config_parser
 from services.date.date_service import millisec_to_date_string, millisec_to_date_with_name_of_the_week
 from services.youtrack import sprint_service
 from services.youtrack.workitem_service import calculate_workitem_duration_in_hours, create_work_items_per_day_dict, get_workitems_for_sprint
+from colorama import Fore
+
+def get_color_based_on_hours(durationHours):
+    if durationHours < 8:
+        return Fore.LIGHTWHITE_EX
+    return Fore.GREEN;
 
 config = config_parser.parse_youtrack_config("src/config/dlouhy.ini");
 
@@ -20,7 +26,8 @@ allhours = 0;
 for key in workitems_per_day:
     durationInHours = calculate_workitem_duration_in_hours(workitems_per_day[key]);
     allhours += durationInHours;
-    print(key + ": " + str(durationInHours) + "h ")
+    print(Fore.CYAN + key + ": " + get_color_based_on_hours(durationInHours) + str(durationInHours) + "h ")
+    
 
 
-print("All: " + str(allhours) + "h")
+print(Fore.CYAN +  "All: " + str(allhours) + "h")
